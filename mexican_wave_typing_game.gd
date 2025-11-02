3e85d89cca8e51618285f3f5a957e99a56d86ad0
conor-wilson <conorwilson1998@gmail.com>
2025-11-02 20:18:15 +1100

    Added custom difficulty settings to mexican wave game

--- mexican_wave_typing_game.gd
+++ mexican_wave_typing_game.gd
@@ -4,8 +4,11 @@ extends Node2D
 @onready var player_crowd_line: CrowdLine = $PlayerCrowdLine
 @onready var enemy_crowd_line: CrowdLine = $EnemyCrowdLine
 
+var challenge_time:float = 5
+
 enum State {
 	WAITING,
+	READY,
 	PLAYING,
 	VICTORY,
 	LOSS,
@@ -15,23 +18,32 @@ var state:State
 func _ready() -> void:
 	reset()
 
-
 func reset() -> void:
 	state = State.WAITING
+	$Ready.hide()
+	$GO.hide()
+	$WelcomeScreen.show()
+	$WinScreen.hide()
+	$LossScreen.hide()
+
+func ready_up():
+	state = State.READY
 	player_crowd_line.accepting_inputs = true
 	player_crowd_line.reset()
 	enemy_crowd_line.reset()
 	$Ready.show()
 	$GO.hide()
+	$WelcomeScreen.hide()
 	$WinScreen.hide()
 	$LossScreen.hide()
+	
 
 func _on_player_crowd_line_go() -> void:
 	state = State.PLAYING
 	$Ready.hide()
 	$GO.show()
 	$GO/BlinkTimer.start()
-	enemy_crowd_line.execute_wave(3)
+	enemy_crowd_line.execute_wave(challenge_time)
 
 func process_win():
 	print("YOU WIN!")
@@ -48,6 +60,8 @@ func process_loss() -> void:
 	print("YOU LOSE!")
 	state = State.LOSS
 	player_crowd_line.stop()
+	enemy_crowd_line.execute_wave(1)
+	await enemy_crowd_line.done_auto_wave
 	$LossScreen.show()
 
 
@@ -63,5 +77,17 @@ func _on_blink_timer_timeout() -> void:
 	$GO.hide()
 
 
-func _on_button_pressed() -> void:
-	reset()
+
+func _on_h_scroll_bar_value_changed(value: float) -> void:
+	$WelcomeScreen/CenterContainer/VBoxContainer/InputLabel.text = str(value) + " seconds"
+	challenge_time = value
+
+
+func _on_ready_button_pressed() -> void:
+	if state == State.WAITING:
+		ready_up()
+
+
+func _on_retry_button_pressed() -> void:
+	if state == State.VICTORY || state == State.LOSS:
+		reset()
