276fcdec2b7865c97d803182bc1c37232292d5e6
conor-wilson <conorwilson1998@gmail.com>
2025-11-06 10:10:49 +1100

    Added some UI elements to start of endless runner game

--- scenes/endless_runner/endless_runner_game.gd
+++ scenes/endless_runner/endless_runner_game.gd
@@ -8,10 +8,30 @@ var moving:bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	reset()
+
+func reset() -> void:
+	moving = false
+	$Ready.show()
+	$GO.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if moving:
 		camera.position += Vector2(1,0)*delta*CAMERA_SPEED
+
+func start():
+	moving = true
+	$Ready.hide()
+	$GO.show()
+	$GO/BlinkTimer.start()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		start()
+
+
+
+func _on_blink_timer_timeout() -> void:
+	$GO.hide()
