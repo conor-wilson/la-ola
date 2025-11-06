4ffd2c33719317b2f439b41ec09af1bf3e93b41c
conor-wilson <conorwilson1998@gmail.com>
2025-11-06 11:19:48 +1100

    Renamed letter_pool to letter_queue. More accurate name.

--- scenes/endless_runner/typable_crowd_row.gd
+++ scenes/endless_runner/typable_crowd_row.gd
@@ -1,29 +1,26 @@
 class_name TypableCrowdRow extends CrowdRow
 
-@export var letter_pool:String
+@export var letter_queue:String
 
-#var current_letter_index:int = 0
 
-func reset_with_new_letter_pool(new_letter_pool:String):
-	letter_pool = new_letter_pool
+func reset_with_new_letter_queue(new_letter_queue:String):
+	letter_queue = new_letter_queue
 	reset()
 
 func reset():
-	#current_letter_index = 0
 	super.reset()
 
 func _spawn_new_crowd_member():
 	var new_crowd_member:CrowdMember = super._spawn_new_crowd_member()
 	
-	if letter_pool == "":
+	if letter_queue == "":
 		# TODO: Here we could generate new letters for the pool
 		return
 	
 	# Setup the new CrowdMember's sign visuals
 	new_crowd_member.has_sign = true
-	new_crowd_member.letter = letter_pool[0]
+	new_crowd_member.letter = letter_queue[0]
 	new_crowd_member.reset()
 	
 	# Pop the letter off of the pool
-	letter_pool = letter_pool.substr(1)
-	#current_letter_index += 1
+	letter_queue = letter_queue.substr(1)
