be3bd95a99e434732bfde99770b8c56f57fcf8d3
Clicksurfer <amit.chameides@gmail.com>
2025-11-28 18:48:09 +1100

    Set their poses

--- scenes/ui/tutorial_popup.gd
+++ scenes/ui/tutorial_popup.gd
@@ -1,7 +1,10 @@
 class_name TutorialPopup extends GamePopup
 
+const PERSON = "Person"
+
 @export var _wave_people: Array[Control] = []
 @export var _sleeping_people: Array[Control] = []
+@export var _sad_people: Array[Control] = []
 
 func _ready() -> void:
     _run_wave_people_gif()
@@ -13,11 +16,13 @@ func _run_wave_people_gif():
 
 func _run_sleeping_people_gif():
     _assign_text_to_group("Sleep", _sleeping_people)
+    _sleeping_people[2].get_node(PERSON).go_to_sleep()
 
 func _run_sad_people_gif():
-    print("Not implemented yet.")
+    for i in range(_sad_people.size()):
+        _sad_people[i].get_node(PERSON).become_upset()
 
 func _assign_text_to_group(text:String, group:Array[Control]):
     var count = min(group.size(), text.length())
     for i in range(count):
-        group[i].get_node("Person").give_letter(text[i])
+        group[i].get_node(PERSON).give_letter(text[i])
