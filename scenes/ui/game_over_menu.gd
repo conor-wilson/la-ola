6dfa88c3372c4e70b9eee5ecdf6607c80684cb84
conor-wilson <conorwilson1998@gmail.com>
2025-12-02 02:04:35 +1100

    Added name text box to gameover screen

--- scenes/ui/game_over_menu.gd
+++ scenes/ui/game_over_menu.gd
@@ -2,8 +2,6 @@ class_name GameOverMenu extends GamePopup
 
 @export var _retry_button:Button
 @export var _main_menu_button:Button
-@export var _score_text:Label
-@export var _high_score_text:Label
 
 @export var _leaderboard_entry_container:Node
 @export var _leaderboard_entry_scene:PackedScene
@@ -15,12 +13,13 @@ const LEADERBOARD_ENTRIES_CAP = 3
 
 # Opens the popup, connecting up the provided button functionality.
 func open_popup(game_controller:GameController, score:int, highscore: int):
-	_score_text.text = SCORE_LABEL_PREFIX + str(score)
-	_high_score_text.text = SCORE_HIGH_LABEL_PREFIX + str(highscore)
+	
 	if _retry_button != null and !_retry_button.pressed.is_connected(game_controller.restart):
 		_retry_button.pressed.connect(game_controller.restart)
+	
 	if _main_menu_button != null and !_main_menu_button.pressed.is_connected(game_controller.quit):
 		_main_menu_button.pressed.connect(game_controller.quit)
+	
 	_init_leaderboard()
 	show()
 
